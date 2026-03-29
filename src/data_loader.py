1e3fda708dc16902f8d3823278447906a5162303
kalyan <kalyansagar7008@gmail.com>
2026-03-29 14:46:29 +0530

    fix: chaged the source file

--- src/data_loader.py
+++ src/data_loader.py
@@ -4,8 +4,8 @@ import os
 def load_data():
     base_path = os.path.dirname(os.path.dirname(__file__))
     
-    train_path = os.path.join(base_path, "RawData", "train_SCkXwSx_cNdUk28_icK39vy_36Oa6WQ.csv")
-    test_path = os.path.join(base_path, "RawData", "test_opI5Wmb_N886n9d_eE68Ob0_dOXWldY.csv")
+    train_path = os.path.join(base_path, "RawData", "train.csv")
+    test_path = os.path.join(base_path, "RawData", "test.csv")
     
     train = pd.read_csv(train_path)
     test = pd.read_csv(test_path)
